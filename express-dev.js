4627ce7f4bc2ebf4857668e90aaf229814f9bb07
青Va <871820453@qq.com>
2021-03-09 21:03:41 +0800

    express实现dev-server，未完成接口有问题，可能与版本有关

---
+++ express-dev.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const webpackDevMid = require('webpack-dev-middleware');
+const webpackHotMid = require('webpack-hot-middleware');
+const webpack = require('webpack');
+const app =express()
+const config = require('./webpack.config.js');
+Object.keys(config).forEach(name=>{
+	config.entry[name] = ['webpack-hot-middleware/client?noInfo=true&reload=true'].concat(config.entry[name])
+})
+const compiler = webpack(config);
+
+app.use(webpackDevMid(compiler,{}));
+app.use(webpackHotMid(compiler,{
+	overlayStyles:true
+}));
+
+app.listen(2021)
